27e16fb1abe95a4b4927a14850b1b6e5aaf10aef
imdavyking <yungdynamic53@gmail.com>
2025-07-15 21:11:17 +0100

    chore: updated

--- contracts/contracts/USDC.sol
+++ contracts/contracts/USDC.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract USDC is ERC20 {
     error USDC__NotOwner();
