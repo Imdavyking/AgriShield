a480e32f0e864f69091086aa635fa130fa2b624d
imdavyking <yungdynamic53@gmail.com>
2025-07-30 18:03:23 +0100

    chore: updated

--- frontend/src/views/plans/plan.tsx
+++ frontend/src/views/plans/plan.tsx
@@ -1,8 +1,35 @@
 import { ellipsify } from "../../utils/ellipsify";
+import { FaSpinner } from "react-icons/fa";
+import { toast } from "react-toastify";
+import {
+  payForInsurance,
+  rethrowFailedResponse,
+} from "../../services/blockchain.services";
+import { NATIVE_TOKEN } from "../../utils/constants";
+import { useState } from "react";
 const formatDate = (unix: string | number) => {
   return new Date(+unix * 1000).toLocaleDateString();
 };
 const Plan = ({ plan }: { plan: any }) => {
+  const [isProcessing, setIsProcessing] = useState(false);
+  const payInsurance = async () => {
+    try {
+      // Implement the payment logic here
+      setIsProcessing(true);
+      console.log(`Paying for plan ${plan.id}`);
+      const response = await payForInsurance({
+        policyId: plan.id,
+        token: NATIVE_TOKEN, // Assuming
+      });
+      rethrowFailedResponse(response);
+      toast.success("Payment successful!");
+    } catch (error) {
+      console.error("Error processing payment:", error);
+      toast.error(error instanceof Error ? error.message : "Payment failed");
+    } finally {
+      setIsProcessing(false);
+    }
+  };
   return (
     <div
       key={plan.id}
@@ -25,8 +52,15 @@ const Plan = ({ plan }: { plan: any }) => {
       <p className="text-gray-700 mb-4">
         <span className="font-semibold">Amount:</span> ${plan.amountInUsd}
       </p>
-      <button className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700 transition cursor-pointer">
-        Pay
+      <button
+        className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700 transition cursor-pointer"
+        onClick={payInsurance}
+      >
+        {isProcessing ? (
+          <FaSpinner className="animate-spin w-5 h-5" />
+        ) : (
+          "Pay Now"
+        )}
       </button>
     </div>
   );
